3541e51f8a91e6326be6074b3100db593cc6f16e
nguyentt1503.jp <nguyentt1503.jp@gmail.com>
2023-10-05 23:05:04 +0700

    update themeleaf

---
+++ project-java/spring-mvc/springmvcthymeleaf/src/main/java/local/springmvcwiththemeleaf/config/AppConfig.java
@@ -0,0 +1,37 @@
+package local.springmvcwiththemeleaf.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+@ComponentScan(basePackages = {"local.springmvcwiththemeleaf"})
+public class AppConfig {
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(viewResolver().getApplicationContext());
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(true);
+        return templateResolver;
+    }
+
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setTemplateEngineMessageSource();
+        return templateEngine;
+    }
+
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        viewResolver.setViewNames(new String[]{".html", ".xhtml"});
+        return viewResolver;
+    }
+}
